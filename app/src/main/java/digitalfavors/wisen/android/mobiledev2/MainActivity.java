a4341f58f93cec21505fe184cfa64c2f3488e3bf
Norris Wise Jr <wisen@WiseNN-MBP.gsu.edu>
2018-06-06 09:24:03 -0400

    First Commit - adding new project with minimal UI

---
+++ app/src/main/java/digitalfavors/wisen/android/mobiledev2/MainActivity.java
@@ -0,0 +1,13 @@
+package digitalfavors.wisen.android.mobiledev2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
